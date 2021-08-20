87d6c13f9803dde6123bf7a327715424bf910ffb
raquelddt <raquel.dotta02@gmail.com>
2021-08-20 00:05:40 +0000

    aula 02

---
+++ app.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, abort , render_template, flash
+
+# configurção
+
+DATABASE = 'blog.db'
+SECRET_KEY = 'pudim'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def conectar_bd():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def antes_requisicao():
+    g.bd = conectar_bd()
+
+app.teardown_request
+def depois_request(exc):
+    g.bd.close() 
+
+@app.route('/')
+def exibir_entradas():
+    return 'Aqui estarão as postagens!!'
+
+
+@app.route('/hello')
+
+def pagina_inicial():
+    return 'Hello World'
+
